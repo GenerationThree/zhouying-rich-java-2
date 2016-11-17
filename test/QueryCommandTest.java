1fecd3e5ca0ad62fe56ea5c518a2898a5a29c1e6
Ying Felix Zhou <ybzhou@thoughtworks.com>
2016-11-17 16:57:39 +0800

    Finish Query Command test with mock

---
+++ test/QueryCommandTest.java
@@ -0,0 +1,25 @@
+import com.sun.tools.javac.util.Pair;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class QueryCommandTest {
+
+    private Player player;
+    Command query;
+
+    @Before
+    public void before() {
+        player = new Player();
+        query = new QueryCommand();
+    }
+
+    @Test
+    public void should_wait_for_command_after_query_command() {
+        Pair<Player.Status, Message> ret = player.execute(query);
+        assertThat(player.getStatus(), is(Player.Status.WAIT_FOR_COMMAND));
+        assertThat(ret.snd, is(Message.SHOW_QUERY_INFO));
+    }
+}
