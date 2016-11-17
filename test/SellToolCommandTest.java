9a8c08fd7c45c0cf3e84f3adb0bef33f30f6e24a
Ying Felix Zhou <ybzhou@thoughtworks.com>
2016-11-17 16:29:34 +0800

    Finish sellTool command test with mock

---
+++ test/SellToolCommandTest.java
@@ -0,0 +1,35 @@
+import com.sun.tools.javac.util.Pair;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SellToolCommandTest {
+    private Player player;
+    private Command sellTool;
+    private Tool tool;
+
+    @Before
+    public void before() {
+        player = new Player();
+        tool = Tool.Bomb;
+        sellTool = new SellToolCommand(tool);
+    }
+
+    @Test
+    public void should_wait_for_command_when_sell_tool_success() {
+        // first, player buy this tool
+        player.buyTool(tool);
+        assertThat(player.getStatus(), is(Player.Status.WAIT_FOR_COMMAND));
+        Pair<Player.Status, Message> ret = player.execute(sellTool);
+        assertThat(ret.snd, is(Message.SELL_TOOL_SUCCESS));
+    }
+
+    @Test
+    public void should_wait_for_command_when_no_that_tool_to_sell() {
+        assertThat(player.getStatus(), is(Player.Status.WAIT_FOR_COMMAND));
+        Pair<Player.Status, Message> ret = player.execute(sellTool);
+        assertThat(ret.snd, is(Message.NO_THAT_TOOL_FOR_SELL));
+    }
+}
