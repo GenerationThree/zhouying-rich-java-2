3d5aede4ebc02dc5291f9ac386cdbbfc2b3c5a4b
Ying Felix Zhou <ybzhou@thoughtworks.com>
2016-11-18 15:36:53 +0800

    Refactor cases of coming to prison to non-reponse place and add cases of comint to prison to it

--- src/rich/place/MineralEstate.java
+++ src/rich/place/MineralEstate.java
@@ -5,8 +5,19 @@ import rich.Message;
 import rich.Player;
 
 public class MineralEstate extends Place {
+    private int points;
+
+    public MineralEstate(int points) {
+        this.points = points;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
     @Override
     public Pair<Player.Status, Message> actionTo(Player player) {
+        player.gainPoints(points);
         return new Pair<>(Player.Status.END_TURN, Message.COME_TO_MINERAL_ESTATE);
     }
 }
--- test/rich/integration/NoResponsePlaceTest.java
+++ test/rich/integration/NoResponsePlaceTest.java
@@ -9,6 +9,7 @@ import rich.Player;
 import rich.command.Command;
 import rich.command.RollCommand;
 import rich.game.GameConstant;
+import rich.place.MineralEstate;
 import rich.place.Prison;
 import rich.place.Starting;
 
@@ -18,36 +19,45 @@ import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class PrisonTest {
+public class NoResponsePlaceTest {
     private GameMap map;
-
     private Player player;
-
-    private Command roll;
     private int step;
     private Starting starting;
-    private Prison prison;
 
     @Before
     public void before() {
         step = 1;
         starting = new Starting();
-        prison = new Prison();
 
         map = mock(GameMap.class);
         when(map.getStarting()).thenReturn(starting);
-        when(map.move(eq(starting), eq(step))).thenReturn(prison);
-
         player = new Player(map, 0);
-        roll = new RollCommand(map, step);
     }
 
     @Test
     public void should_end_turn_and_add_wait_times_when_come_to_prison() {
+        Prison prison = new Prison();
+        when(map.move(eq(starting), eq(step))).thenReturn(prison);
+        Command roll = new RollCommand(map, step);
+
         Pair<Player.Status, Message> commandRet = player.execute(roll);
         assertThat(player.getStatus(), is(Player.Status.END_TURN));
         assertThat(commandRet.snd, is(Message.COME_TO_PRISON));
         assertThat(player.getWaitTimes(), is(GameConstant.DAYS_IN_PRISON));
+    }
+
+    @Test
+    public void should_end_turn_and_add_points_when_come_to_mineral_estate() {
+        final int POINT_IN_MINERAL = 20;
+        MineralEstate mineralEstate = new MineralEstate(POINT_IN_MINERAL);
+        when(map.move(eq(starting), eq(step))).thenReturn(mineralEstate);
+        Command roll = new RollCommand(map, step);
+
+        Pair<Player.Status, Message> commandRet = player.execute(roll);
+        assertThat(player.getStatus(), is(Player.Status.END_TURN));
+        assertThat(commandRet.snd, is(Message.COME_TO_MINERAL_ESTATE));
+        assertThat(player.getCurrentPoints(), is(POINT_IN_MINERAL));
     }
 
 }
